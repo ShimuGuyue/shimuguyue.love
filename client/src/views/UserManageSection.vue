5b9b32e5db8817bedb1b68b3e869d663be10aa6c
ShimuGuyue <shimuguyue513@qq.com>
2026-08-13 22:00:11 +0800

    Update 新建用户按钮移到一级菜单

--- client/src/views/UserManageSection.vue
+++ client/src/views/UserManageSection.vue
@@ -280,14 +280,6 @@ async function saveChanges() {
             type="button"
             class="manage-btn manage-btn--primary"
             :disabled="saving"
-            @click="openCreateDialog"
-          >
-            新建用户
-          </button>
-          <button
-            type="button"
-            class="manage-btn manage-btn--primary"
-            :disabled="saving"
             @click="saveChanges"
           >
             {{ saving ? '保存中...' : '保存编辑' }}
@@ -301,14 +293,22 @@ async function saveChanges() {
             取消编辑
           </button>
         </template>
-        <button
-          v-else
-          type="button"
-          class="manage-btn manage-btn--primary"
-          @click="startEdit"
-        >
-          编辑用户
-        </button>
+        <template v-else>
+          <button
+            type="button"
+            class="manage-btn manage-btn--primary"
+            @click="openCreateDialog"
+          >
+            新建用户
+          </button>
+          <button
+            type="button"
+            class="manage-btn manage-btn--primary"
+            @click="startEdit"
+          >
+            编辑用户
+          </button>
+        </template>
       </div>
     </div>
 
